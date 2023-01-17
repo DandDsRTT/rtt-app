a4572d72972210dcbe912e0e6b964551540350f3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2023-01-17 10:57:26 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const root: HTMLDivElement = document.createElement("div")
+document.body.appendChild(root)
+
+const input: HTMLDivElement = document.createElement("input")
+
+root.appendChild(input)
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const IgnoreNotFoundExportPlugin = require("ignore-not-found-export-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.ts",
+    devtool: "inline-source-map",
+    devServer: {
+        static: {
+            directory: "dist",
+            watch: true,
+        },
+    },
+    watchOptions: {
+        ignored: ["node_modules", "spec"],
+    },
+    resolve: {
+        extensions: [".ts", ".scss", ".js", ".json"],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "RTT app",
+            meta: {viewport: "width=device-width, height=device-height, initial-scale=1, maximum-scale=1"},
+        }),
+        new IgnoreNotFoundExportPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.[tj]s$/,
+                loader: "ts-loader",
+                options: {
+                    compilerOptions: {
+                        module: "esnext",
+                    },
+                    transpileOnly: true,
+                },
+            },
+        ],
+    },
+}
