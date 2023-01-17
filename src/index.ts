3094aa823b262f39a76994ef0495736f9be2b65f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2023-01-17 11:28:33 -0800

    v1.0.2: basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+import axios from "axios"
+
 const root: HTMLDivElement = document.createElement("div")
 document.body.appendChild(root)
 
-const input: HTMLDivElement = document.createElement("input")
-
+const input: HTMLInputElement = document.createElement("input")
+input.value = "optimizeGeneratorTuningMap?unparsedT=[⟨1 1 0] ⟨0 1 4]}&tuningSchemeSpec=TILT minimax-U"
+input.style.width = "700px"
 root.appendChild(input)
+
+const submit: HTMLButtonElement = document.createElement("button")
+submit.innerText = "Submit"
+root.appendChild(submit)
+
+const results: HTMLDivElement = document.createElement("div")
+root.appendChild(results)
+
+const HOST = "https://rtt-lib.onrender.com/"
+
+submit.onclick = () => {
+    results.innerText = "Loading..."
+    axios.get(
+        HOST + encodeURI(input.value),
+        {},
+    ).then(data => {
+        console.log("you tried to send", data.data)
+        results.innerText = data.data
+    }).catch(e => {
+        console.log("axios error: ", e)
+    })
+}
